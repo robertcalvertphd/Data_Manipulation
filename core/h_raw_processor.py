0b637a31fbb7b5971325b7df5d517a48c6e1db8c
robertcalvertphd <robert.calvert.phd@gmail.com>
2020-05-28 15:28:03 -0700

    v4:
    Gui up and running.
    Rasch and 2 parameter established
    Passing score calculations

--- core/h_raw_processor.py
+++ core/h_raw_processor.py
@@ -17,6 +17,7 @@ def make_folders(name, parent_folder):
         os.mkdir(project_folder + "/xCalibreOutput")
         os.mkdir(project_folder + "/keys")
     except:
+        return 1
         print("Error creating folders. Confirm that they already exist.\n Folder: " + project_folder)
 
 
@@ -28,36 +29,36 @@ def ui_process_raw_data():
 
 
 def process_raw_data(name, parent_folder, raw_data):
-    make_folders(name, parent_folder)
+    a = make_folders(name, parent_folder)
     project_folder = parent_folder+"/"+name
-    pair_data_and_pro_exam(raw_data, project_folder)
-
+    b = pair_data_and_pro_exam(raw_data, project_folder)
+    return a,b
 
 def process_paired_files(pe_file, data_file, project_folder, select_Domain = False):
     #   todo: add in domain processing here so that sets without domain names are still separated
     pe_df = hfh.get_df(pe_file,header=0)
     pe_df.Position = pe_df.Position.astype(float)
     pe_df = pe_df.sort_values("Position", ascending=True, )
-#    pe_df = pe_df[["AccNum",'CorrectAnswer','Domain']]
-    pe_df = pe_df[["AccNum", 'CorrectAnswer']]
+    try:
+        pe_df = pe_df[["AccNum",'CorrectAnswer','Domain']]
+    except:
+        return 0, "csv file is missing AccNum or CorrectAnswer or Domain."
+        pe_df = pe_df[["AccNum", 'CorrectAnswer']]
 #    pe_df["AccNum"] = pe_df["AccNum"] + "_" + pe_df["Domain"]
 
     pe_df['number_of_options'] = 4
     pe_df['group'] = 1
     pe_df["include"] = 'Y'
 
-
     if select_Domain:
-    #    pe_df.loc[pe_df.Domain != select_Domain, 'include'] = 'N'
-        print("needs to be implemented but different sets have different names")
+        pe_df.loc[pe_df.Domain != select_Domain, 'include'] = 'N'
+
     pe_df['type'] = 'M'
     processed_path = project_folder+"/processed_data/"
     c_path = processed_path+hfh.get_stem(pe_file)+"_c.csv"
     pe_df.to_csv(c_path, header=False, index = False)
     h.convert_default_data_to_iteman(data_file, processed_path, new_name=hfh.get_stem(pe_file))
-    #Position,AccNum,UseCode,CorrectAnswer,Rubric1,Rubric2,CognitiveLevel,P Value,cPBS,Measure,Corr,Source,Item Status,OnDraftCount,Level,Actual Use,Rubric3,Rubric4,Rubric5,ScenarioAccNum,SeriesPosition,Version,Enemies,QSP,InRange,ExamsCounter,AuthorID,TranslationStamp,Keywords
-    #LAW_75,D,4,1,Y,M
-
+    return 1
 
 def pair_files_by_month_and_year(pe_files, data_files, project_folder):
     for pe_file in pe_files:
@@ -76,6 +77,7 @@ def pair_data_and_pro_exam(raw_data, project_folder, try_again_option=False):
     confirm = u.get_yes_no_response("Matching", confirm_message)
     if confirm:
         pair_files_by_month_and_year(pe_files,data_files, project_folder)
+        return 1
     else:
         try_again = u.get_yes_no_response("Error","No files found. Would you like to try again?")
         if try_again and try_again_option:
